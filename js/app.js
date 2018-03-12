b1e55046a5f6faff9444406e63f74a44fded8cb9
Rhiannon Mortensen <mortensenrhi@gmail.com>
2018-03-12 16:15:55 -0700

    cointuning theme

--- js/app.js
+++ js/app.js
@@ -28,4 +28,72 @@ console.log(pikeLocation);
 pikeLocation.salesPerHour();
 
 
-var seaTacLocation;
+var seaTacLocation = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookieSale: 1.2,
+
+  salesPerHour: function () {
+    var seaTacStore = document.getElementById('seaTacLocation');
+    console.log(seaTacStore);
+
+    for (var i = 0; i < storeHours.length; i++) {
+
+      var cusRandoNum = (Math.random() * (this.maxCustomer - this.minCustomer) + (this.minCustomer));
+
+      var cookiesPerHour = document.createElement('li');
+
+      cookiesPerHour.textContent = storeHours[i] + ': ' + (Math.round(cusRandoNum * this.avgCookieSale));
+
+      seaTacStore.appendChild(cookiesPerHour);
+    }
+  },
+};
+console.log(seaTacLocation);
+seaTacLocation.salesPerHour();
+
+var seattleCenterLocation = {
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookieSale: 3.7,
+
+  salesPerHour: function () {
+    var seattleCenterStore = document.getElementById('seattleCenterLocation');
+
+    for (var i = 0; i < storeHours.length; i++) {
+      var cusRandoNum = (Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+
+      var cookiesPerHour = document.createElement('li');
+
+      cookiesPerHour.textContent = storeHours[i] + ': ' + (Math.round(cusRandoNum * this.avgCookieSale));
+
+      seattleCenterLocation.appendChild(cookiesPerHour);
+    }
+  },
+};
+console.log(seaTacLocation);
+seaTacLocation.salesPerHour();
+
+var capitolHillLocation = {
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCookieSale: 2.3,
+
+  salesPerHour: function () {
+    var capitolHillStore = document.getElementById(capitolHillLocation);
+
+    for (var i = 0; i < storeHours.length; i++) {
+      var cusRandoNum = (Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+
+      var cookiesPerHour = document.createElement('li');
+
+      cookiesPerHour.textContent = storeHours[i] + ': ' + (Math.round(cusRandoNum * this.avgCookieSale));
+
+      capitolHillLocation.appendChild(cookiesPerHour);
+    }
+  },
+};
+console.log(capitolHillLocation);
+capitolHillLocation.salesPerHour();
+
+var alkiLocation;
