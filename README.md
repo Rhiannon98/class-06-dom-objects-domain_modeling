83fcce9d15869136831217da390aab76ad6a2bcf
Rhiannon Mortensen <mortensenrhi@gmail.com>
2018-03-12 15:08:10 -0700

    working format for salePerHour

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # class-06-dom-objects-domain_modeling
 
-Working in a group with Timea, Max, and Jason (also me but I think that is a given).
+Working in a group with Timea, Max, Earl-Jay and Jason (also me but I think that is a given).
 
--- js/app.js
+++ js/app.js
@@ -1,67 +1,31 @@
-//locations
-//store hours
+// //locations
+// //store hours
 
 var storeHours = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 console.log(storeHours);
+
 
 var pikeLocation = {
   minCustomer: 23,
   maxCustomer: 65,
   avgCookieSale: 6.3,
 
-  custPerHour: function () {
-    return Math.random(23, 65);
+  salesPerHour: function () {
+    var pikeStore = document.getElementById('pikeLocation');
+
+    for (var i = 0; i < storeHours.length; i++) {
+
+      var cusRandoNum = (Math.random() * (this.maxCustomer - this.minCustomer) + (this.minCustomer));
+
+      var cookiesPerHour = document.createElement('li');
+      cookiesPerHour.textContent = storeHours[i] + ': ' + (Math.round(cusRandoNum * this.avgCookieSale));
+
+      pikeStore.appendChild(cookiesPerHour);
+    }
   },
-
-  var cookiesPerHour: document.getElementById(''),
-
-  for ( var i = 0;  i < this.storeHours.length; i++ ) {
-    
-    var teamElement = document.createElement('li');
-        teamElement.textContent = years[i] + ": " + this.champions[i];
-    
-    nflContainer.appendChild(teamElement);
-}
-    
-  }
 };
+console.log(pikeLocation);
+pikeLocation.salesPerHour();
 
-var seaTacLocation = {
-  minCustomer: 3,
-  maxCustomer: 24,
-  avgCookieSale: 1.2,
 
-  custPerHour: function () {
-    return Math.random(3, 24);
-  }
-};
-
-var seattleCenterLocation = {
-  minCustomer: 11,
-  maxCustomer: 38,
-  avgCookieSale: 2.3,
-
-  custPerHour: function () {
-    return Math.random(11, 38);
-  }
-};
-
-var capitolHillLocation = {
-  minCustomer: 20,
-  maxCustomer: 38,
-  avgCookieSale: 2.3,
-
-  custPerHour: function () {
-    return Math.random(20, 38);
-  }
-};
-
-var alkiLocation = {
-  minCustomer: 2,
-  maxCustomer: 16,
-  avgCookieSale: 4.6,
-
-  custPerHour: function () {
-    return Math.random(2, 16);
-  }
-};
+var seaTacLocation;
