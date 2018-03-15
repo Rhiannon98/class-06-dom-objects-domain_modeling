408002f1710f2ca2a37567f1f535caa368965b8d
Rhiannon Mortensen <mortensenrhi@gmail.com>
2018-03-15 14:50:11 -0700

    moved pics. new css.

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ var header = function () {
     //append tr to thead
     //append thead to table
 
-    thElement.textContent = storeHours[i];
+    thElement.textContent = storeHours[i] + '00';
 
     trElement.appendChild(thElement);
   }
@@ -190,7 +190,7 @@ function addNewStore(event) {
 
   var newMinCus = event.target.minCustomer.value;
   var newMaxCus = event.target.maxCustomer.value;
-  var newAvgCookieSale = event.target.avgCookieSale.value;
+  var newAvgCookieSale = event.target.avgCookieSales.value;
   var newStoreLocal = event.target.storeLocation.value;
 
   new StoreConstructor(newMinCus, newMaxCus, newAvgCookieSale, newStoreLocal);
